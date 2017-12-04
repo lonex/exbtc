9379c83e546c1de49558381a32586a7c07614ad6
lonex <stonelonely@gmail.com>
2017-12-03 19:00:58 -0800

    ex_doc, publish

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exbtc.Mixfile do
   def project do
     [
       app: :exbtc,
-      description: "Exlixir ECC utils, port from python Bitcoin tool",
+      description: "Exlixir ECC (Elliptic curve cryptography) utils, port from Python Bitcoin tool (https://github.com/vbuterin/pybitcointools)",
       package: package(),
       version: "0.1.0",
       elixir: "~> 1.5",
@@ -24,8 +24,7 @@ defmodule Exbtc.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
   end
 
